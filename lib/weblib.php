f1656dfe642e9a4aae3fe963b788c67a52583366
cfulton <cfulton@clamp-it.org>
2011-01-08 12:15:52 -0800

    MDL-25695: allow anchor tags

--- lib/weblib.php
+++ lib/weblib.php
@@ -1530,6 +1530,7 @@ function purify_html($text, $options = array()) {
         $config->set('HTML.Doctype', 'XHTML 1.0 Transitional');
         $config->set('URI.AllowedSchemes', array('http'=>true, 'https'=>true, 'ftp'=>true, 'irc'=>true, 'nntp'=>true, 'news'=>true, 'rtsp'=>true, 'teamspeak'=>true, 'gopher'=>true, 'mms'=>true));
         $config->set('Attr.AllowedFrameTargets', array('_blank'));
+        $config->set('Attr.EnableID', true);
 
         if (!empty($CFG->allowobjectembed)) {
             $config->set('HTML.SafeObject', true);
